c4b518dc65097e854c0c70a6cf74df22bd7adc95
Neeraj <you@example.com>
2021-10-29 20:50:57 +0530

    Categories lammbda function

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var axios = require('axios')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const BASE_URL = `https://www.cubyt.io/data/categories`
+
+async function getCategories(){
+    const response = await axios({
+        method:"GET",
+        url : BASE_URL,
+        headers: {
+            
+        },
+        params:{
+
+        }
+    })
+    return response.data
+}
+
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+
+app.get('/data/categories/:catName', async function(req, res) {
+  const categories=await getCategories()   
+  const filteredData=categories.filter((item,index)=>item.category_name==req.params.catName)
+  res.send(filteredData)
+});
+
+app.get('/data/categories',async function(req, res) {
+  const categories=await getCategories()   
+  res.send(categories)
+});
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+module.exports = app
